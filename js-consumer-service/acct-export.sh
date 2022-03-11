1ee53c7604ff2e5968de8f6ae3eb764942fd5ce1
Todd Beets <todd@beetsme.net>
2022-03-11 14:48:02 -0800

    Added --response-type "Stream" to CONSUMER.NEXT exports

--- js-consumer-service/acct-export.sh
+++ js-consumer-service/acct-export.sh
@@ -14,12 +14,12 @@ ACCTCPUBKEY="ABTJUJC5DHOPQHI2WLUDUBTYDDNJKP45J7SWUDPP673TK3X7U5FUGU33"
 
 addexports () {
 # ACCTA public export FULFILLEVENTS (any account may import)
-nsc add export --account $ACCTA --name "FULFILLEVENTS-GRANT-NEXT" --subject "\$JS.API.CONSUMER.MSG.NEXT.FULFILLEVENTS.FULFILLEVENTS-C1" --service
+nsc add export --account $ACCTA --name "FULFILLEVENTS-GRANT-NEXT" --subject "\$JS.API.CONSUMER.MSG.NEXT.FULFILLEVENTS.FULFILLEVENTS-C1" --service --response-type "Stream"
 nsc add export --account $ACCTA --name "FULFILLEVENTS-GRANT-ACK" --subject "\$JS.ACK.FULFILLEVENTS.FULFILLEVENTS-C1.>" --service
 nsc add export --account $ACCTA --name "FULFILLEVENTS-GRANT-INFO" --subject "\$JS.API.CONSUMER.INFO.FULFILLEVENTS.FULFILLEVENTS-C1" --service
 
 # ACCTC private export RESTOCKEVENTS
-nsc add export --private --account $ACCTC --name "RESTOCKEVENTS-GRANT-NEXT" --subject "\$JS.API.CONSUMER.MSG.NEXT.RESTOCKEVENTS.RESTOCKEVENTS-C1" --service
+nsc add export --private --account $ACCTC --name "RESTOCKEVENTS-GRANT-NEXT" --subject "\$JS.API.CONSUMER.MSG.NEXT.RESTOCKEVENTS.RESTOCKEVENTS-C1" --service --response-type "Stream"
 nsc add export --private --account $ACCTC --name "RESTOCKEVENTS-GRANT-ACK" --subject "\$JS.ACK.RESTOCKEVENTS.RESTOCKEVENTS-C1.>" --service
 nsc add export --private --account $ACCTC --name "RESTOCKEVENTS-GRANT-INFO" --subject "\$JS.API.CONSUMER.INFO.RESTOCKEVENTS.RESTOCKEVENTS-C1" --service
 
